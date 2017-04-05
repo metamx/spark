9149cfb800d4baa96ec7e43d8716ac228810ebc1
gkc2104 <kamal.eagles2104@gmail.com>
2017-04-04 17:04:25 -0700

    [SPARK-4899][MESOS] Support for checkpointing on Coarse and Fine grained schedulers

--- resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosFineGrainedSchedulerBackend.scala
+++ resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosFineGrainedSchedulerBackend.scala
@@ -78,8 +78,8 @@ private[spark] class MesosFineGrainedSchedulerBackend(
       sc.appName,
       sc.conf,
       sc.conf.getOption("spark.mesos.driver.webui.url").orElse(sc.ui.map(_.webUrl)),
-      Option.empty,
-      Option.empty,
+      sc.conf.getOption("spark.mesos.checkpoint"),
+      sc.conf.getOption("spark.mesos.failoverTimeout"),
       sc.conf.getOption("spark.mesos.driver.frameworkId")
     )
 
